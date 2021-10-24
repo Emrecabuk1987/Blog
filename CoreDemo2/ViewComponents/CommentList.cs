285cd96d75d4390582363b303d9f997d51cdc1e2
Emrecabuk1987 <pentagonmc1@gmail.com>
2021-10-25 00:49:51 +0300

    30.Ders Sonu

---
+++ CoreDemo2/ViewComponents/CommentList.cs
@@ -0,0 +1,36 @@
+﻿using CoreDemo2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo2.ViewComponents
+{
+     public class CommentList: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    ID=1,
+                    Username="Emre"
+                },
+                new UserComment
+                {
+                    ID =2,
+                    Username="Mesut"
+
+                },
+                new UserComment
+                {
+                    ID=3,
+                    Username="Merve"
+                }
+            };
+            return View(commentvalues);
+        }
+    }
+}
