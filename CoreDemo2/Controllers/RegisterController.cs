219cc2450629b6fc1f939dfaed0e1d07ba41a007
Emrecabuk1987 <pentagonmc1@gmail.com>
2021-10-28 01:03:17 +0300

    33.Ders Sonu

---
+++ CoreDemo2/Controllers/RegisterController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo2.Controllers
+{
+    public class RegisterController : Controller
+    {
+        //ekleme işlemi yapılırken httpget ve httppost attribute'lerinin tanımlandığı
+        //metodların isimileri aynı olmak zorundadır.
+        //httpget sayfa yüklenince çalışmaya başlıyor.
+        //httppost sayfada buton tetiklenince.
+        //httpget attribute komutu sayfada kategorize veya benzeri işlemler kullanılırken
+        //sayfa yüklendiği anda listelenmesi istenilen niteliklerde kullanılabilir.
+        //iller - ilçeler httpget yazılabilir ,
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index()
+        {
+
+        }
+    }
+}
